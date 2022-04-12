88d3973a202024e201eaf33e429aae7c3a1e8f76
lixg <lixg95@126.com>
2022-04-12 10:45:02 +0800

    revive

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
-python train.py --seed 10 --gpu 0 & sleep 1s
-python train.py --seed 20 --gpu 1 & sleep 1s
+# python train.py --seed 10 --gpu 0 & sleep 1s
+# python train.py --seed 20 --gpu 1 & sleep 1s
 wait
-python train.py --seed 30 --gpu 0 & sleep 1s
-python train.py --seed 40 --gpu 1 & sleep 1s
+# python train.py --seed 30 --gpu 0 & sleep 1s
+# python train.py --seed 40 --gpu 1 & sleep 1s
 wait
-python train.py --seed 50 --gpu 0 & sleep 1s
-python train.py --seed 60 --gpu 1 & sleep 1s
+# python train.py --seed 50 --gpu 0 & sleep 1s
+# python train.py --seed 60 --gpu 1 & sleep 1s
 wait
-python train.py --seed 70 --gpu 0 & sleep 1s
+# python train.py --seed 70 --gpu 0 & sleep 1s
 python train.py --seed 80 --gpu 1 & sleep 1s
 wait
-python train.py --seed 90 --gpu 0 & sleep 1s
+# python train.py --seed 90 --gpu 0 & sleep 1s
 python train.py --seed 100 --gpu 1 & sleep 1s
 echo "END"
