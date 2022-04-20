7c98a46cb75d4b711f2a48f16cef54e79851b3c5
lixg <lixg95@126.com>
2022-04-20 08:27:32 +0800

    gp改为torch

--- Models/gp.py
+++ Models/gp.py
@@ -26,10 +26,16 @@ class GaussianProcess:
 
         # makes sure the features have an additional testue for the bias term
         # We call the features H since the features are used as the basis functions h(x)
-        H_train = np.concatenate((feat_train, np.ones((feat_train.shape[0], 1))), axis=1)
-        H_test = np.concatenate((feat_test, np.ones((feat_test.shape[0], 1))), axis=1)
+        # H_train = np.concatenate((feat_train, np.ones((feat_train.shape[0], 1))), axis=1)
+        # H_test = np.concatenate((feat_test, np.ones((feat_test.shape[0], 1))), axis=1)
+        feat_train = torch.from_numpy(feat_train)
+        feat_test = torch.from_numpy(feat_test)
+
+        H_train = torch.cat((feat_train, torch.ones((feat_train.shape[0], 1))), dim=1)
+        H_test = torch.cat((feat_test, torch.ones((feat_test.shape[0], 1))), dim=1)
 
         Y_train = np.expand_dims(train_yield, axis=1)
+        Y_train = torch.from_numpy(Y_train)
 
         n_train = feat_train.shape[0]
         n_test = feat_test.shape[0]
@@ -43,26 +49,33 @@ class GaussianProcess:
         se_loc = squareform(pdist(locations, 'euclidean')) ** 2 / (self.r_loc ** 2)
         se_year = squareform(pdist(years, 'euclidean')) ** 2 / (self.r_year ** 2)
 
-        # make the dirac matrix we'll add onto the kernel function
-        noise = np.zeros([n_train + n_test, n_train + n_test])
-        noise[0: n_train, 0: n_train] += (self.sigma_e ** 2) * np.identity(n_train)
+        se_loc = torch.from_numpy(se_loc)
+        se_year = torch.from_numpy(se_year)
 
-        kernel = ((self.sigma ** 2) * np.exp(-se_loc) * np.exp(-se_year)) + noise
+        # make the dirac matrix we'll add onto the kernel function
+        noise = torch.zeros([n_train + n_test, n_train + n_test])
+        # noise[0: n_train, 0: n_train] += (self.sigma_e ** 2) * np.identity(n_train)
+        noise[0: n_train, 0: n_train] += (self.sigma_e ** 2) * torch.eye(n_train)
+        kernel = ((self.sigma ** 2) * torch.exp(-se_loc) * torch.exp(-se_year)) + noise
+        kernel = kernel.to(torch.float32)
+
 
         # since B is diagonal, and B = self.sigma_b * np.identity(feat_train.shape[1]),
         # its easy to calculate the inverse of B
-        B_inv = np.identity(H_train.shape[1]) / self.sigma_b
+        B_inv = torch.eye(H_train.shape[1]) / self.sigma_b
         # "We choose b as the weight vector of the last layer of our deep models"
-        b = np.concatenate((model_weights.transpose(1, 0), np.expand_dims(model_bias, 1)))
-        K_inv = np.linalg.inv(kernel[0: n_train, 0: n_train])
+        b = torch.cat((model_weights.transpose(1, 0), torch.from_numpy(np.expand_dims(model_bias, 1))))
+        K_inv = torch.linalg.inv(kernel[0: n_train, 0: n_train]).to(torch.float32)
         # The definition of beta comes from equation 2.41 in Rasmussen (2006)
-        m1 = torch.tensor(B_inv + H_train.T.dot(K_inv).dot(H_train))
-        beta = torch.linalg.inv(m1).numpy().dot(
-            H_train.T.dot(K_inv).dot(Y_train) + B_inv.dot(b))
+        # print(H_train.dtype,K_inv.dtype ,B_inv.dtype,b.dtype,Y_train.dtype)
+        beta = torch.linalg.inv(B_inv + H_train.T.mm(K_inv).mm(H_train)).mm(
+            H_train.T.mm(K_inv).mm(Y_train) + B_inv.mm(b))
         # We take the mean of g(X*) as our prediction, also from equation 2.41
-        pred = H_test.dot(beta) + \
-               kernel[n_train:, :n_train].dot(K_inv).dot(Y_train - H_train.dot(beta))
+        pred = H_test.mm(beta) + kernel[n_train:, :n_train].mm(K_inv).mm(Y_train - H_train.mm(beta))
+
         return pred
+
+    
 
 
 
@@ -133,7 +146,7 @@ class gp_model(GaussianProcess):
         # self.model_weight = None
         # self.model_bias = None
 
-        return torch.squeeze(torch.tensor(gp_pred))
+        return torch.squeeze(gp_pred)
 
 
     def clear_testing_params(self):
