0b296be60ce66d99a452d45727eb23b25de3b942
lixg <lixg95@126.com>
2022-07-04 11:33:20 +0800

    删除过去的sh运行文件

--- run_1_fold.sh
+++
@@ -1,6 +0,0 @@
-CFG=swint
-TAG=$1
-FOLD=$2
-python main.py run_1_fold ${CFG}.yaml ${TAG} ${FOLD}
-
-# python post_analysis.py get_logs ${CFG}_${TAG}
--- run_all.sh
+++
@@ -1,4 +0,0 @@
-TAG=$1
-
-python main.py run_all swint.yaml ${TAG}
-python main.py get_logs swint_${TAG}
